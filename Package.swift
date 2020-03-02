1448424d839dfdb25cca1ef72b5ff4d758bda6fd
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-03-02 16:28:23 -0600

    Updated to the latest Swift package format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftHexTools"
+    name: "SwiftHexTools",
+    platforms: [.macOS(.v10_15)],
+    products: [
+        .library(name: "SwiftHexTools", targets: ["SwiftHexTools"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftHexTools",
+            dependencies: []
+        )
+    ]
 )
